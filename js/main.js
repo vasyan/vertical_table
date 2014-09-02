a3a5c478831e5325d098f44e97ac9459d240f58d
Vasiliy Styazhkin <vasiliy.styazhkin@resumup.com>
2014-09-03 01:03:46 +0400

    firefox/opera svg text params tuning

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ window.onload = function() {
   var htmlElement = document.getElementsByTagName("html")[0];
   htmlElement.className = htmlElement.className.replace(/no-js/, "");
 
-  // var rotates = document.getElementsByClassName("rotate");
-  // for (var i = 0; i < rotates.length; i++) {
-  //   rotates[i].style.height = rotates[i].offsetWidth + 'px';
-  // }
   var classesList = htmlElement.className;
   if (classesList.match(/no-csstransitions/) && classesList.match(/[^(no\-)]svg/)) {
     var template = function(template, data) {
@@ -17,43 +13,48 @@ window.onload = function() {
       var svgTemplate = "data:image/svg+xml;charset/utf-8," +
         "<svg xmlns=\"http://www.w3.org/2000/svg\">" +
         "<text x=\"{{x}}\" y=\"{{y}}\" style=\"font-family:{{fontFamily}};" +
-        "color: black;font-size:{{fontSize}}px; text-anchor:middle\" transform=\"rotate(-90)\">{{textValue}}</text>" +
+        "font-size:{{fontSize}}px; font-weight:{{fontWeight}}; text-anchor:finish\" transform=\"rotate(-90)\">{{textValue}}</text>" +
         "</svg>",
-
-        fontSize = 12,
-        fontFamily = "Arial",
         objElement = document.createElement("object"),
-        thList = table.getElementsByTagName("th"),
-        textContainer,
+        divsList = table.getElementsByTagName("thead")[0].getElementsByTagName("div"),
+        computedStyles = window.getComputedStyle(divsList[0], null),
+        fontFamily = computedStyles.getPropertyValue("font-family"),
+        fontSize = computedStyles.getPropertyValue("font-size"), //truly font-size
+        fontWeight = computedStyles.getPropertyValue("font-weight"),
+        length = divsList.length,
+        originalHeadersWidth = [],
         textValue,
-        spanElem,
         height,
-        type,
+        width,
+        elem,
         obj,
         i;
 
-      for(i = 0; i < thList.length; i++) {
-        textContainer = thList[i].getElementsByTagName("*");
-        textContainer = textContainer[(textContainer.length - 1)];
-        textValue = textContainer.innerHTML;
-        height = textContainer.clientHeight;
-        width = textContainer.clientWidth;
+      for(i = 0; i < length; i++) {
+        originalHeadersWidth.push(divsList[i].clientWidth);
+      }
+
+      for(i = 0; i < length; i++) {
+        elem = divsList[i];
+        textValue = elem.innerHTML;
+        height = elem.clientHeight;
+        width = originalHeadersWidth[i];
         obj = objElement.cloneNode(true);
 
 
         obj.height = (height > width) ? height : width;
         obj.type = "image/svg+xml";
         obj.width = fontSize;
-        // obj.data = "data:image/svg+xml;charset/windows-1251,<svg xmlns='http://www.w3.org/2000/svg'><text x='" + (- obj.height/2) + "' y='" + fontSize + "' style='font-family:" + fontFamily + "; font-size:" + fontSize + "px; text-anchor:middle' transform='rotate(-90)'>" + text + "</text></svg>";
         obj.data = template(svgTemplate, {
-          x: -(obj.height/2),
+          x: -(obj.height),
           y: fontSize,
           fontSize: fontSize,
           fontFamily: fontFamily,
           textValue: textValue,
+          fontWeight: fontWeight
         });
 
-        thList[i].replaceChild(obj, thList[i].firstChild);
+        divsList[i].replaceChild(obj, divsList[i].firstChild);
       }
 
     };
